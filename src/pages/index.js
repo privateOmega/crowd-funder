3c339d7541412b07d8c0b9c048f8297ce36a6d50
privateOmega <privateOmega@protonmail.com>
2020-02-19 00:12:17 +0530

    adding components to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,33 @@
 import React from "react"
 import { Link } from "gatsby"
+import { makeStyles } from "@material-ui/core"
+import classNames from "classnames"
 
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </>
-)
+import indexPagestyles from "../styles/index-page"
+
+const useStyles = makeStyles(indexPagestyles)
+
+function IndexPage() {
+  const classes = useStyles()
+  return (
+    <>
+      <SEO title="Home" />
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <div className={classes.container}>
+          <h1>Hi people</h1>
+          <p>Welcome to your new Gatsby site.</p>
+          <p>Now go build something great.</p>
+          <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+            <Image />
+          </div>
+          <Link to="/page-2/">Go to page 2</Link>
+        </div>
+      </div>
+    </>
+  )
+}
 
 export default IndexPage
