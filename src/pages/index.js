8a5434e93d8c7e0904ad53f42bcbe0259b1e135b
privateOmega <privateOmega@protonmail.com>
2020-02-20 02:36:34 +0530

    added content in the main section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { makeStyles } from "@material-ui/core"
+import { makeStyles, LinearProgress } from "@material-ui/core"
 import classNames from "classnames"
 
 import SEO from "../components/seo"
@@ -38,7 +38,29 @@ function IndexPage() {
         </div>
       </Parallax>
       <div className={classNames(classes.main, classes.mainRaised)}>
-        <div className={classes.container}>Main content</div>
+        <div className={classes.container}>
+          <div className={classes.section}>
+            <GridContainer justify="center">
+              <GridItem xs={12} sm={12} md={8}>
+                <h2 className={classes.sectionTitle}>Let's talk product</h2>
+                <h5 className={classes.description}>
+                  This is the paragraph where you can write more details about
+                  your product. Keep you user engaged by providing meaningful
+                  information. Remember that by this time, the user is curious,
+                  otherwise he wouldn't scroll to get here. Add a button if you
+                  want the user to see more.
+                </h5>
+              </GridItem>
+            </GridContainer>
+          </div>
+          <div className={classes.section}>
+            <GridContainer justify="center">
+              <GridItem xs={12} sm={12} md={8}>
+                <LinearProgress variant="determinate" color="primary" value={60} />
+              </GridItem>
+            </GridContainer>
+          </div>
+        </div>
       </div>
     </>
   )
--- src/styles/index-page.js
+++ src/styles/index-page.js
@@ -31,6 +31,20 @@ const landingPageStyle = {
     boxShadow:
       "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)",
   },
+  section: {
+    padding: "70px 0",
+    textAlign: "center",
+  },
+  sectionTitle: {
+    ...title,
+    marginBottom: "1rem",
+    marginTop: "30px",
+    minHeight: "32px",
+    textDecoration: "none",
+  },
+  description: {
+    color: "#999",
+  },
 }
 
 export default landingPageStyle
