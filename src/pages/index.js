684c9b87689bb6e9ea18785e671e14454ce21c19
privateOmega <privateomega@protonmail.com>
2020-02-19 11:36:53 +0530

    updated index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
-import { Link } from "gatsby"
 import { makeStyles } from "@material-ui/core"
 import classNames from "classnames"
 
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Parallax from "../components/parallax"
+import GridContainer from "../components/grid-container"
+import GridItem from "../components/grid-item"
+import Button from "../components/button"
 
 import indexPagestyles from "../styles/index-page"
 
@@ -12,19 +14,29 @@ const useStyles = makeStyles(indexPagestyles)
 
 function IndexPage() {
   const classes = useStyles()
+
   return (
     <>
       <SEO title="Home" />
-      <div className={classNames(classes.main, classes.mainRaised)}>
+      <Parallax filter image={require("../images/image.jpg")}>
         <div className={classes.container}>
-          <h1>Hi people</h1>
-          <p>Welcome to your new Gatsby site.</p>
-          <p>Now go build something great.</p>
-          <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-            <Image />
-          </div>
-          <Link to="/page-2/">Go to page 2</Link>
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={6}>
+              <h1 className={classes.title}>E-bike.</h1>
+              <h4>
+                An electric bicycle also known as an e-bike is a bicycle with an
+                integrated electric motor which can be used for propulsion.
+              </h4>
+              <br />
+              <Button href="/login" color="danger">
+                Donate
+              </Button>
+            </GridItem>
+          </GridContainer>
         </div>
+      </Parallax>
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <div className={classes.container}>Main content</div>
       </div>
     </>
   )
