f1e29a90de3fb2f3a56dd08b7b8d9c9f9f2b1521
privateOmega <privateOmega@protonmail.com>
2020-02-20 23:21:34 +0530

    Updates

---
+++ component---src-pages-login-js-32c9ea15b0f232954034.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{SGa5:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),l=a("Wbzz"),c=a("vrFN");t.default=function(){return o.a.createElement(o.a.Fragment,null,o.a.createElement(c.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-login-js-32c9ea15b0f232954034.js.map
